51b607142f94e1fdd37ee8ba9c75eb4b4e43fd17
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-05-02 12:07:35 -0300

    style: implemente styled Components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,10 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Title } from "./styles";
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Olá Groups</Text>
-    </View>
+    <Container>
+      <Title>Olá Groups</Title>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
