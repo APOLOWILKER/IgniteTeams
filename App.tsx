77d0ea94b7d0b46266aeb6debf692e5efdb9312b
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-05-02 16:13:47 -0300

    feat: add and configure Status Bar

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
 
 import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
   // logic charging fronts - asynchronously
@@ -14,6 +15,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
       { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
