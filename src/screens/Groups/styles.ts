c58d5267d66db66e7665e52af745bf3ae2109aa4
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-05-02 13:03:24 -0300

    style: change import for styled type and include /native

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -5,12 +5,6 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background-color: ${({theme}) => theme.COLORS.GRAY_600};
-  align-items: center;
-  justify-content: center;
+  padding: 24px;
 `;
 
-export const Title = styled.Text`
-  font-size: 32px;
-  color: #fff;
-  font-weight: bold;
-`;
