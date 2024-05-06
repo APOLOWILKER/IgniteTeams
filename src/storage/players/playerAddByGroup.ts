3f6167357706db3f30741459cc645dc3233b3949
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-05-06 15:41:36 -0300

    feat: create group for teams

---
+++ src/storage/players/playerAddByGroup.ts
@@ -0,0 +1,32 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AppError } from "@utils/AppError";
+import { PLAYER_COLLECTION } from "../storageConfig";
+import { PlayerStorageDTO } from "./PlayerStorageDTO";
+import { playersGetByGroup } from "./playersGetByGroup";
+
+
+export async function playerAddByGroup(newPlayer: PlayerStorageDTO, group: string) {
+  try {
+    /**
+     Exemplo: 
+      @ignite-team: players-rocket: []
+      @ignite-team: players-amigos: []
+      @ignite-team: players-ignite: []
+     */
+    
+    const storedPlayers = await playersGetByGroup(group);
+
+    const playerAlreadyExists = storedPlayers.filter(player => player.name === newPlayer.name);
+
+    if(playerAlreadyExists.length > 0) {
+      throw new AppError('Essa pessoa ja está adicionada em um time aqui.')
+    }
+
+    const storage = JSON.stringify([...storedPlayers, newPlayer])
+
+    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, storage)
+
+  } catch (error) {
+    throw (error);
+  }
+}
